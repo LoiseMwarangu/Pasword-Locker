c28c567381b0c32404fea15a7bb31573b715d4ab
Loise Mwarangu <loisemwarangu@gmail.com>
2019-01-26 23:43:55 +0300

    added login to password locker

---
+++ user.py
@@ -0,0 +1,16 @@
+username= "its just me"
+password= "emtsujsti"
+
+print("Hello there, welcome to password locker")
+print()
+
+userUsername= input("Enter your username:")
+userPassword= input("Enter your password:")
+
+while username != userUsername or password != userPassword:
+    print("you entered a wrong username and/or password")
+    userUsername= input("Enter your username:")
+    userPassword= input("Enter your password:")
+
+print("You are now logged in")
+
