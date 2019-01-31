375c451da0d2d404779228f1ff416f92afe04d9d
Loise Mwarangu <loisemwarangu@gmail.com>
2019-01-31 10:01:40 +0300

    added statements to run.py file to check user input and call the functions based on users choice

--- run.py
+++ run.py
@@ -67,3 +67,75 @@ def main():
             print("\n")
             print(f"New Account **{user_name}** created.\n")
 
+        elif short_code == "cc":
+            print("\nLogin to your account")
+            print("."*40)
+            print("\nUsername?")
+            print("." * 20)
+            username_input = input()
+            print("\nPassword?")
+            print("."*20)
+            user_password_input = input()
+            view_password = user_password_input
+            if check_existing_users(user_password_input):
+                print("\nWelcome back!")
+                print("New Credential")
+                print("." *20)
+
+                print("\nWhich account do the credentials belong to?")
+                print("."*40)
+                account = input()
+
+                print(f"\nWhat's your login name for the {account} account?")
+                print("."*40)
+                login_name = input()
+
+                print("\nChoose:")
+                print("."*20)
+                print("'gp' - program to generate your password for you, 'cp' - create your own password")
+                password_creation_input = input()
+                if password_creation_input == "cp":
+                    print("\nEnter your password")
+                    print("."*20)
+                    pass_word = input()
+                elif password_creation_input == "gp":
+                    chars = "abcdefghijklmnopqrstuvwxyz1234567890"
+                    pass_word = "".join(random.choice(chars) for _ in range(8))
+                    print(f"\nYour password is: **{pass_word}**")
+
+                save_credentials(create_credentials(view_password,account,login_name,pass_word))
+                print("\n")
+                print(f"New credentials **{account}**, **{login_name}**, **{pass_word}** created")
+
+            else:
+                print("Wrong password or username. Please Try again.\n Username?")
+                print("."*20)
+                username_input = input()
+                print("\nPassword?")
+                print("."*20)
+                password_input = input()
+                if check_existing_users(user_password_input):
+                    print("\nWelcome back!")
+                else:
+                    print("You don't have an account.\n")
+
+        elif short_code == 'dc':
+            if display_credentials():
+                print("Here is a list of your credentials:")
+                print("."*40)
+
+                for credential in display_credentials():
+                    print(f"\nAccount: {credential.account}\nLogin Name: {credential.login}\nAccount Password: {credential.password}")
+            else:
+                print("\n You don't seem to have any credentials saved yet")
+
+        elif short_code == 'ex':
+            print("."*50)
+            print("Thank you for using Password Locker...")
+            print("."*50)
+            break
+
+        else:
+            print("Sorry, I didn't get that. Please use the short codes\n")
+
+
